76bc36786e1dd44e608abca34506fd632884d86d
Paulo Lopes Estevão <pl1745240@gmail.com>
2021-08-08 00:10:53 +0100

    edit: connection db

--- utils/db.go
+++ utils/db.go
@@ -1,12 +1,13 @@
 package utils
 
 import (
+	"log"
+	"os"
+
 	"github.com/Paulo-Lopes-Estevao/go_graphql_postegres/graph/model"
 	"github.com/jinzhu/gorm"
 	"github.com/joho/godotenv"
-	"log"
-	"os"
-	_"github.com/lib/pq"
+	_ "github.com/lib/pq"
 )
 
 func ConnectDB() *gorm.DB {
@@ -14,15 +15,15 @@ func ConnectDB() *gorm.DB {
 	err := godotenv.Load()
 
 	if err != nil {
-		log.Fatalf("Error     loading .env file %v",err)
+		log.Fatalf("Error     loading .env file %v", err)
 	}
 
 	dsn := os.Getenv("dsn")
 
-	db, err := gorm.Open("postgres",dsn)
+	db, err := gorm.Open("postgres", dsn)
 
 	if err != nil {
-		log.Fatalf("Error  connect to datebase %v",err)
+		log.Fatalf("Error  connect to datebase %v", err)
 		panic(err)
 	}
 
